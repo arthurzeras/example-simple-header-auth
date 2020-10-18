a24b649158af3c7d129a33ffe24f2d65c8dbd595
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-10-17 23:45:11 -0300

    feat(login): Cria formulario de login basico

---
+++ src/views/login/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="login">
+    <input type="password" v-model="password" placeholder="password">
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    password: '',
+  }),
+
+  methods: {
+    authenticate() {
+      console.log(this.password);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
